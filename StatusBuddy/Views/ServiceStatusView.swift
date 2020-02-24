00afd3cf75a93ce6dcb154a253c4b33d4e47185e
Sickenberg <romain.sickenberg@liip.ch>
2020-02-24 10:40:11 +0100

    Enhanced the fonts on list and Added Start + End date separately

--- StatusBuddy/Views/ServiceStatusView.swift
+++ StatusBuddy/Views/ServiceStatusView.swift
@@ -14,20 +14,29 @@ struct ServiceStatusView: View {
 
     init(_ service: Service) {
         self.service = service
+        print(service.eventMessage)
     }
     
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
                 Text(service.serviceName)
+                    .font(.system(size: 12.5))
+                    .foregroundColor(Color(.textColor))
 
                 if !service.events.isEmpty {
                     Text(service.eventMessage)
                         .foregroundColor(Color(.secondaryLabelColor))
                         .font(.system(size: 11))
-                        .lineLimit(2)
+                        .fixedSize(horizontal: false, vertical: true)
+                    Text(service.eventStartDate)
+                        .foregroundColor(Color(.secondaryLabelColor))
+                        .font(.system(size: 11))
+                    Text(service.eventEndDate)
+                        .foregroundColor(Color(.secondaryLabelColor))
+                        .font(.system(size: 11))
                 }
-            }.padding([.top, .bottom], 4)
+            }.padding([.top, .bottom], (service.events.isEmpty ? 4 : 8))
             Spacer()
             Circle()
                 .frame(width: 10, height: 10, alignment: .center)
